b73f1f1172c72b17e712e624136a92cefa774908
Grant <GrantWils.23@googlemail.com>
2022-01-26 23:22:21 +0000

    add tutorial content to video file of section-02 / 07 writing our first jquery script

--- 02-jQueryIntroduction/07-writing_our_first_jquery_script/video_solution/js/myscript.js
+++ 02-jQueryIntroduction/07-writing_our_first_jquery_script/video_solution/js/myscript.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    
+    $("h2").css("text-decoration", "underline");  //underlines all <h2> elements
+    $("ul").css("border", "solid 2px #ccc");  //adds a border to all lists
 });
+
+$(document).ready(function() {
+    $("h2").addClass("underline");  //underlines all <h2> elements
+    $("ul").addClass("border");  //adds a border to all lists
+});
+
+
+$(document).ready(function() {
+    $("h2").removeClass("underline");  //underlines all <h2> elements
+    $("ul").removeClass("border");  //adds a border to all lists
+});
+
+
